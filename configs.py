e034ae9a74790242f35870cd9543315399966231
tiffany <tiff0207@bu.edu>
2025-04-04 00:32:04 -0400

    changed reload param, faster detect

--- configs.py
+++ configs.py
@@ -11,7 +11,8 @@ def get_absolute_path(relative_path):
     base_path = os.path.dirname(__file__)
     return os.path.abspath(os.path.join(base_path, relative_path))
 
-def load_config(reload_targetlist=True):
+# def load_config(reload_targetlist=True):
+def load_config(reload_targetlist=False):
 
     with open(os.path.join(os.path.dirname(__file__), 'configs/configs.yaml')) as file:
         configs = yaml.load(file, Loader=yaml.FullLoader)
